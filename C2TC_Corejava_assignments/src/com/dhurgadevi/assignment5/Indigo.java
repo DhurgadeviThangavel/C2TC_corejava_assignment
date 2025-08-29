fdf0fa6efdb93b8d68c6b6721e19b53563b4ada1
OMEN <OMEN@LAPTOP-G6BSE1K2>
2025-08-29 09:24:40 +0530

    Commit

--- C2TC_Corejava_assignments/src/com/dhurgadevi/assignment5/Indigo.java
+++ C2TC_Corejava_assignments/src/com/dhurgadevi/assignment5/Indigo.java
@@ -38,3 +38,5 @@ package com.dhurgadevi.assignment5;
 	    }
 	}
 
+
+	
---
+++ C2TC_Corejava_assignments/src/com/dhurgadevi/assignmet6/InsufficientFundsException.java
@@ -0,0 +1,7 @@
+package com.dhurgadevi.assignmet6;
+
+public class InsufficientFundsException extends Exception {
+	 public InsufficientFundsException(String message) {
+	     super(message);
+	 }
+	}
